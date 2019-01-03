ca7ad12242e4fff517dc759c3e7d8b2535aaa6cd
mc222sn <mc222sn@student.lnu.se>
2019-01-03 20:12:53 +0100

    Adding code

--- src/js/clock/clock.js
+++ src/js/clock/clock.js
@@ -1,69 +1,12 @@
 import { Window } from '../window/window.js'
-
-const clockDiv = document.createElement('template')
-clockDiv.innerHTML = `
-<style>
-  a {
-    color: inherit;
-  }
-  
-  a:hover,
-  a:active {
-    color: inherit;
-  }
-
-  .nav {
-    margin-bottom: 1em;
-  }
-
-  #time,
-  #stopwatch-time {
-    font-size: 5rem;
-  }
-
-  .input-group {
-    margin-bottom: 1em;
-  }
-</style>
-<ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="stopwatch-tab" data-toggle="tab" href="#stopwatch" role="tab" aria-controls="stopwatch" aria-selected="true">Stopwatch</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="timer-tab" data-toggle="tab" href="#timer" role="tab" aria-controls="timer" aria-selected="false">Timer</a>
-  </li>
-</ul>
-
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="stopwatch" role="tabpanel" aria-labelledby="stopwatch-tab">
-    <p id="stopwatch-time"></p>
-    <button id="stopwatch-start-btn" class="btn btn-primary" type="submit">Start</button>
-    <button id="stopwatch-reset-btn" class="btn btn-light" type="submit">Reset</button>
-  </div>
-  <div class="tab-pane fade" id="timer" role="tabpanel" aria-labelledby="timer-tab">
-    <p id="time"></p>
-    
-    <div id="time-input" class="input-group">
-      <div class="input-group-prepend">
-        <span class="input-group-text">Time: </span>
-      </div>
-      <input type="text" aria-label="Hours" class="form-control" placeholder="Hours">
-      <input type="text" aria-label="Minutes" class="form-control" placeholder="Minutes">
-      <input type="text" aria-label="Seconds" class="form-control" placeholder="Seconds">
-    </div>
-
-    <button id="timer-btn" class="btn btn-primary" type="submit">Start</button>
-    <button id="timer-new-btn" class="btn btn-light" type="submit">Reset</button>
-  </div>
-</div>
-`
+import template from './template.js'
 
 export class Clock extends Window {
   constructor () {
     super()
     this.modalIcon.setAttribute('src', '/image/nav/clock.png')
     this.modalIcon.setAttribute('alt', 'Clock')
-    this.modalBody.appendChild(clockDiv.content.cloneNode(true))
+    this.modalBody.appendChild(template.content.cloneNode(true))
     
     this.runStopwatch = true
     this.runTimer = true
