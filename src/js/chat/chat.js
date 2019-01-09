a81cb274e6ae0b5035c6146e14cccbd4ec763eed
mc222sn <mc222sn@student.lnu.se>
2019-01-09 19:18:29 +0100

    Chaning Chat module

--- src/js/chat/chat.js
+++ src/js/chat/chat.js
@@ -34,9 +34,10 @@ export class Chat extends Window {
     this.newUsername = this.appBody.querySelector('#new-username')
     this.chatSettings = this.appBody.querySelector('#settings')
 
+    // Checks what theme setting is set in localstorage
     if (window.localStorage.getItem('settings')) {
       let settings = JSON.parse(window.localStorage.getItem('settings'))
-    
+
       if (settings.theme === 'dark-theme') {
         this.app.appendChild(settingsStyle.content.cloneNode(true))
         this.appBody.querySelector('#dark-theme').checked = true
@@ -53,6 +54,10 @@ export class Chat extends Window {
       }
     }
 
+    /**
+     *  If username do not exist in localstorage
+     *  remove #username-form hidden attribute.
+     */
     if (!window.localStorage.getItem('user')) {
       this.appBody.querySelector('#username-form').hidden = false
     } else {
@@ -63,6 +68,10 @@ export class Chat extends Window {
 
     const socket = new WebSocket(this.url)
 
+    /**
+     *  Listen if a new message has been recieved
+     *  and if so, add it to the DOM.
+     */
     socket.addEventListener('message', event => {
       let msg = JSON.parse(event.data)
 
@@ -77,6 +86,9 @@ export class Chat extends Window {
       }
     })
 
+    /**
+     *  Listen for input in the channel input form.
+     */
     this.appBody.addEventListener('input', event => {
       event.preventDefault()
 
@@ -114,6 +126,10 @@ export class Chat extends Window {
       }
     })
 
+    /**
+     *  Listen for change in the checkbox toggle
+     *  what theme should be used.
+     */
     this.chatSettings.addEventListener('change', event => {
       if (event.target.checked) {
         let theme = 'dark-theme'
@@ -122,7 +138,7 @@ export class Chat extends Window {
       } else {
         let theme = 'light-theme'
         window.localStorage.setItem('settings', JSON.stringify({ theme: theme }))
-   
+
         let appDiv = this.shadowRoot.querySelector('#app')
         let styles = appDiv.querySelectorAll('style')
 
@@ -134,6 +150,10 @@ export class Chat extends Window {
       }
     })
 
+    /**
+     *  Listen for submit with new username and
+     *  sets it in localstorage.
+     */
     this.newUsername.addEventListener('submit', event => {
       event.preventDefault()
 
@@ -146,6 +166,9 @@ export class Chat extends Window {
       }
     })
 
+    /**
+     *  Listen for click on the nav items.
+     */
     this.navigation.addEventListener('click', event => {
       event.preventDefault()
 
@@ -164,10 +187,16 @@ export class Chat extends Window {
       }
     })
 
+    /**
+     *  Listen for click on the header close button and close socket.
+     */
     this.appHeaderCloseBtn.addEventListener('click', event => {
       socket.close()
     })
 
+    /**
+     *  Listen for click on the footer close button and close socket.
+     */
     this.appFooterCloseBtn.addEventListener('click', event => {
       socket.close()
     })
